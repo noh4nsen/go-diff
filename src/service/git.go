96159ca81cf2863ca35655e84dc20c068941a1a0
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-07 15:28:25 -0300

    feat: new method that fetch specified branches

--- src/service/git.go
+++ src/service/git.go
@@ -9,29 +9,42 @@ import (
 )
 
 func ExecDiff(args []string) model.Changes {
+	execFetch()
+
 	cmd := exec.Command("git", "diff", "--name-only", args[1], args[2])
 
 	output, err := cmd.Output()
 	helper.CheckAndLogError("Error executing 'git diff' command: %v", err)
+	log.Println("Executed: 'git diff --name-only", args[1], args[2], "'")
 
 	return core.ExtractChanges(string(output))
 }
 
 func CheckGitInstallation() {
 	gitPath, err := exec.LookPath("git")
-
 	helper.CheckAndLogError("Git is not installed on the system:%v", err)
 	log.Println("Git is installed: ", gitPath)
 }
 
 func CheckBranchesExists(args []string) {
-	for index, value := range args {
-		if index != 0 {
-			cmd := exec.Command("git", "rev-parse", "--verify", value)
-
-			output, err := cmd.Output()
-			helper.CheckAndLogError("Error verifying branch! %v", err)
-			log.Println("Branch: ", string(output))
+	for index, branch := range args {
+		if index > 0 && index < 3 {
+			execRevParse(branch)
 		}
 	}
+}
+
+func execFetch() {
+	cmd := exec.Command("git", "fetch")
+	_, err := cmd.Output()
+	helper.CheckAndLogError("Error executing 'git fetch' command: %v", err)
+	log.Println("Executed: 'git fetch'")
+}
+
+func execRevParse(branch string) {
+	cmd := exec.Command("git", "rev-parse", "--verify", branch)
+
+	output, err := cmd.Output()
+	helper.CheckAndLogError("Error verifying branch! %v", err)
+	log.Println("Hash: ", string(output))
 }
