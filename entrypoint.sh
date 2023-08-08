b0e23f547c4f6aca9b0fbcc70e0f2d257e46881b
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-08 18:00:40 -0300

    feat: split outputs on action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-output=$(./go-diff $1 $2)
-echo $output
-echo "modified_output=output" >> $GITHUB_OUTPUT
+output=$(/go-diff $1 $2) 
+modified_files=$(echo $output | jq .modifiedFiles)
+modified_dirs=$(echo $output | jq .modifiedDirs)
+modified_projects=$(echo $output | jq .modifiedProjects)
+
+echo "modified_files="$modified_files"" >> $GITHUB_OUTPUT
+echo "modified_dirs="$modified_dirs"" >> $GITHUB_OUTPUT
+echo "modified_projects="$modified_projects"" >> $GITHUB_OUTPUT
