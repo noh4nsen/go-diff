686f841383347d6da6e80fcb8cec2858fdf6146e
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-09 12:30:43 -0300

    feat: included full go-diff full json output as output option on action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -eoux pipefail
+
 output=$(/go-diff $1 $2) 
+
 modified_files=$(echo $output | jq .modifiedFiles)
-modified_dirs=$(echo $output | jq .modifiedDirs)
+modified_dirs=$(echo $outut | jq .modifiedDirs)
 modified_projects=$(echo $output | jq .modifiedProjects)
 
+echo "json_output=$output" >> $GITHUB_OUTPUT
 echo "modified_files="$modified_files"" >> $GITHUB_OUTPUT
 echo "modified_dirs="$modified_dirs"" >> $GITHUB_OUTPUT
 echo "modified_projects="$modified_projects"" >> $GITHUB_OUTPUT
